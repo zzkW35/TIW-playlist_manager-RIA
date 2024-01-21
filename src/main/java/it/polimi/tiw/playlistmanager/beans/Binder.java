c9caed6cb563b6f936bc549c86e41562c17f987d
Filippo Roggi <zzkw35@gmail.com>
2024-01-22 00:12:29 +0100

    Add basic Binder bean and DAO

---
+++ src/main/java/it/polimi/tiw/playlistmanager/beans/Binder.java
@@ -0,0 +1,31 @@
+package it.polimi.tiw.playlistmanager.beans;
+
+public class Binder {
+    private int id;
+    private int playlistId;
+    private int songId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public void setSongId(int songId) {
+        this.songId = songId;
+    }
+}
---
+++ src/main/java/it/polimi/tiw/playlistmanager/dao/BinderDAO.java
@@ -0,0 +1,82 @@
+package it.polimi.tiw.playlistmanager.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import it.polimi.tiw.playlistmanager.beans.Binder;
+
+public class BinderDAO {
+private Connection connection;
+
+    public BinderDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Binder findBinderById(int id) throws SQLException {
+        String query = "SELECT * FROM binder WHERE id = ?";
+        Binder binder = null;
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                binder = new Binder();
+                binder.setId(resultSet.getInt("id"));
+                binder.setPlaylistId(resultSet.getInt("playlist_id"));
+                binder.setSongId(resultSet.getInt("song_id"));
+            }
+        }
+        catch (SQLException e) {
+            throw new SQLException("Something went wrong while searching for the binder: " + e.getMessage());
+        }
+        finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            }
+            catch (SQLException e) {
+                throw new SQLException("Something went wrong while closing resultSet: " + e.getMessage());
+            }
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            }
+            catch (SQLException e) {
+                throw new SQLException("Something went wrong while closing preparedStatement: " + e.getMessage());
+            }
+        }
+        return binder;
+    }
+    public void createBinder(int playlistId, int songId) throws SQLException {
+        String query = "INSERT INTO binder (playlist_id, song_id) VALUES (?, ?)";
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, playlistId);
+            preparedStatement.setInt(2, songId);
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new SQLException("Something went wrong while creating the binder: " + e.getMessage());
+        }
+        finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            }
+            catch (SQLException e) {
+                throw new SQLException("Something went wrong while closing preparedStatement: " + e.getMessage());
+            }
+        }
+    }
+}
